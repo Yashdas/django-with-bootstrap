13df24ac2434ffa329cd1664138a30a538377d1a
yash_123 <yashdas1@gmail.com>
2022-01-14 22:21:11 +0530

    my first commt

---
+++ home/views.py
@@ -0,0 +1,150 @@
+from django.shortcuts import render, HttpResponse, redirect
+from home.models import Contact
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from blog.models import Post
+
+# Create your views here.
+def home(request):
+    return render(request, 'home/home.html') 
+
+def about(request): 
+    return render(request, 'home/about.html') 
+
+def contact(request): 
+    if request.method=='POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        print(name, email, phone, content)
+
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<4:
+            messages.error(request, "Please fill the form correctly")
+        else:
+            contact = Contact(name=name, email=email, phone=phone, content=content)
+            contact.save()
+            messages.success(request, "Your message has been successfully sent")
+        
+    return render(request, 'home/contact.html') 
+
+
+def search(request):
+    query = request.GET['query']
+    if len(query)>78:
+        allPosts = Post.objects.none() 
+    else:
+        allPostsTitle = Post.objects.filter(title__icontains=query)
+        allPostsContent = Post.objects.filter(content__icontains=query)
+        allPosts = allPostsTitle.union(allPostsContent)
+    
+    if allPosts.count() == 0:
+         messages.warning(request, "No search results found. Please refine your query")
+    params = {'allPosts': allPosts, 'query':query}
+    return render(request, 'home/search.html', params) 
+
+def handleSignup(request):
+    if request.method == 'POST':
+        # Get the post parameters
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        # Check for errorneous inputs
+        # username should be under 10 characters
+        if len(username) > 10:
+            messages.error(request, "Username must be under 10 characters")
+            return redirect('home')
+        
+        # username should be alphanumeric
+        if not username.isalnum():
+            messages.error(request, "Username should only contain letters and numbers")
+            return redirect('home')
+
+        # passwords should match
+        if pass1 != pass2:
+            messages.error(request, "Passwords do not match")
+            return redirect('home')
+
+        # Create the user 
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, "Your iCoder account has been successfully created")
+        return redirect('home')
+    else:
+        return HttpResponse('404 - Not Found')
+
+def handleLogin(request):
+    if request.method == 'POST':
+        # Get the post parameters
+        loginusername = request.POST['loginusername']
+        loginpassword = request.POST['loginpassword']
+
+        user = authenticate(username=loginusername, password=loginpassword)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In")
+            return redirect('home') 
+        else:
+            messages.error(request, "Invalid Credentials, Please try again")
+            return redirect('home')
+            
+    return HttpResponse('404 - Not Found')
+
+
+def handleLogout(request): 
+    logout(request)
+    messages.success(request, "Successfully Logged Out")
+    return redirect('home') 
+
+def analyze(request):
+    # Get the text
+    djtext = request.POST.get('text', 'default')
+
+    removepunc=request.POST.get('removepunc','off')
+    fullcaps=request.POST.get('fullcaps','off')
+    newlineremover=request.POST.get('newlineremover','off')
+    extraspaceremover=request.POST.get('extraspaceremover','off')
+
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext=analyzed
+        # return render(request, 'analyze.html', params)
+    if fullcaps=="on":
+        analyzed=""
+        for char in djtext:
+            analyzed=analyzed+char.upper()
+        params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
+        djtext=analyzed
+        # return render(request, 'analyze.html', params)
+    if newlineremover=="on":
+        analyzed=""
+        for char in djtext:
+            if char!="\n":
+                analyzed=analyzed+char
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        djtext=analyzed
+        # return render(request, 'analyze.html', params)
+    if (extraspaceremover == "on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not (djtext[index] == " " and djtext[index + 1] == " "):
+                analyzed = analyzed + char
+        params = {'purpose': 'Change To Uppercase', 'analyzed_text': analyzed}
+        djtext=analyzed
+        # return render(request, 'analyze.html', params)
+
+    return render(request, 'home/analyze.html', params)
